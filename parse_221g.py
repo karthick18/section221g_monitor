8b8f91d79393831afc34b910b72c21ea81c3b38e
A R Karthick <a.r.karthick@gmail.com>
2012-06-26 10:04:58 -0700

    Revert "sort the changed cases reports"
    This reverts commit b9d200e17d511ee37020de43602631c2caaadf90.

--- parse_221g.py
+++ parse_221g.py
@@ -37,9 +37,7 @@ def parse_221g(last_txt_file, new_txt_file):
                                           case_status )
 
     print 'Case status changed for [%d] cases\n' %len(changed_cases.keys())
-    items = changed_cases.items()
-    items.sort()
-    for id, status in items:
+    for id, status in changed_cases.items():
         print 'Case [%s] status changed from [%s] to [%s]' %(id, status[0], status[1])
 
     print '\n\nTracked cases status:\n'
