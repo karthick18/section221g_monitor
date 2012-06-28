6f2e60c56b4af437f6c96ec922c3ee68e8d0e5c4
A R Karthick <a.r.karthick@gmail.com>
2012-06-28 06:24:56 -0700

    Change case status body format by including counts per case status, changed tracked case handling, remove parse_221g.py and integrate the changes directly into process_221g.py, include tracked cases hit count in case of tracked case status chaneges in the subject

--- process_221g.py
+++ process_221g.py
@@ -27,6 +27,7 @@ if os.stat(cur_case_name).st_size == 0:
     print 'Unable to fetch case data from [%s]' %section_221g_url
     sys.exit(0)
 
+
 last_case_txt_file = last_case_name.split('.')[0] + '.txt'
 cur_case_txt_file =  cur_case_name.split('.')[0] + '.txt'
 
@@ -34,7 +35,80 @@ os.system('pdftotext -layout %s %s' %(last_case_name, last_case_txt_file))
 os.system('pdftotext -layout %s %s' %(cur_case_name, cur_case_txt_file))
 print 'Processing section 221g case list pertaining to last case file [%s] and current file [%s]' %(
     last_case_txt_file, cur_case_txt_file)
-os.system('./parse_221g.py %s %s > %s' %(last_case_txt_file, cur_case_txt_file, 'case_status'))
 
-send_mail('c3po-z1@openclovis.com', 'karthick@openclovis.com', 'Visa case status', 
-          file('case_status').read())
+tracked_cases = {'2012156-291-1':('PENDING_PROCESS', 'PENDING_PROCESS')}
+def parse_221g(last_txt_file, new_txt_file):
+    last_lines = open(last_txt_file, 'r').readlines()
+    last_cases = {}
+    changed_cases = {}
+    case_status_map = {}
+    case_status_body = ''
+    parse_line = False
+    for l in last_lines:
+        l1 = l.strip()
+        if not parse_line:
+            if l1.find('BATCH ID') >= 0:
+                parse_line = True
+            continue
+
+        ## get case id
+        case_id = '-'.join(l1.split()[:3]).strip()
+        case_status = '_'.join(l1.split()[3:]).strip()
+        last_cases[case_id] = case_status
+        
+    parse_line = False
+    new_lines = open(new_txt_file, 'r').readlines()
+    for l in new_lines:
+        l1 = l.strip()
+        if not parse_line:
+            if l1.find('BATCH ID') >= 0:
+                parse_line = True
+            continue
+
+        case_id =  '-'.join(l1.split()[:3]).strip()
+        case_status = '_'.join(l1.split()[3:]).strip()
+        if case_id in last_cases.keys():
+            if case_status != last_cases[case_id]:
+                changed_cases[case_id] = (last_cases[case_id], 
+                                          case_status )
+                if case_id in tracked_cases.keys():
+                    tracked_cases[case_id] = (last_cases[case_id],
+                                              case_status ) #update tracked case status
+
+                status = case_status.lower()
+                if not case_status_map.has_key(status):
+                    case_status_map[status] = []
+
+                case_status_map[status].append(case_id)
+
+    case_status_body += 'Case status changed for [%d] cases\n' %len(changed_cases.keys())
+    for status in case_status_map.keys():
+        case_status_body += 'Cases %s: [%d]\n' %(status, len(case_status_map[status]))
+    case_status_body += '\n'
+    items = changed_cases.items()
+    items.sort()
+    for id, status in items:
+        case_status_body += 'Case [%s] status changed from [%s] to [%s]\n' %(id, status[0], status[1])
+
+    case_status_body += '\n\nTracked cases status:\n'
+    track_hits = 0
+    for t in tracked_cases.keys():
+        last_status = tracked_cases[t][0]
+        cur_status =  tracked_cases[t][1]
+        if last_status != cur_status:
+            track_hits = track_hits + 1
+            case_status_body += 'Case [%s] being tracked has changed from [%s] to [%s]\n' %(
+                t, last_status, cur_status)
+        else:
+            case_status_body += 'Case [%s] status remains unchanged\n' %t
+
+    case_status_subject = 'Visa case status'
+    if track_hits:
+        case_status_subject += ' (Tracked cases hits: %d)' %track_hits
+
+    return (case_status_subject, case_status_body)
+
+subject, body = parse_221g(last_case_txt_file, cur_case_txt_file)
+# dump the body to a case status file for a sample mail format
+file('case_status', 'w').write(body)
+send_mail('c3po-z1@openclovis.com', 'karthick@openclovis.com', subject, body) 
